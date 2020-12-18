b206d80b1ba8a385e22f559e344568987a1e1057
sandhya563 <60354285+sandhya563@users.noreply.github.com>
2020-12-18 09:10:51 +0530

    Add files via upload

---
+++ lenNestedList.py
@@ -0,0 +1,11 @@
+dict=[[1,2,3],[2,3,4]]
+i=0
+while i <len(dict):
+    j=0
+    count=1
+    while j<len(dict):
+        if dict[i]==dict[i]:
+            count=count+dict[i][j]
+        j=j+1
+    i=i+1
+print(count)
